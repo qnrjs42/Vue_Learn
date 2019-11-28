955d7bbc9ee8c990305796e45866d52c4c21ddc4
BooGeon <qnrjs42@gmail.com>
2019-11-29 03:46:56 +0900

    숫자야구 완성

--- 3.숫자야구/webpack-config.js
+++ 3.숫자야구/webpack-config.js
@@ -1,17 +1,14 @@
 const VueLoaderPlugin = require('vue-loader/lib/plugin');
 const path = require('path');
-const nodeExternals = require('webpack-node-externals');
 // path.join(__dirname, 'main.js')
 // ex) C://Users/myComputer/desktop/vue_learn/3.숫자야구/main.js
 
 module.exports = {
   mode: 'development',
-  devtool: 'eval',
-  target: 'node',
+  devtool: 'eval', // eval build하는 속도 빠름
   resolve: {
     extensions: ['.js', '.vue'],
   },
-  externals: [nodeExternals()],
   entry: {
     app: path.join(__dirname, '/main.js'),
   },
