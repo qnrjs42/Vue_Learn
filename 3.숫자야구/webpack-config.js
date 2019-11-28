30476077d45ed71bc28ef1fa6a8c02244fa51bf9
BooGeon <qnrjs42@gmail.com>
2019-11-29 02:10:51 +0900

    ERROR in Entry module not found: Error: Can't resolve './src' in 'C:\Users\myComputer\Desktop\vue_learn\3.숫자야구'
    에러 해결 방법

--- 3.숫자야구/webpack-config.js
+++ 3.숫자야구/webpack-config.js
@@ -1,19 +1,23 @@
 const path = require('path');
-// path.join(__dirname, './main.js')
+const nodeExternals = require('webpack-node-externals');
+// path.join(__dirname, 'main.js')
 // ex) C://Users/myComputer/desktop/vue_learn/3.숫자야구/main.js
 
 module.exports = {
+  target: 'node',
+  externals: [nodeExternals()],
   entry: {
-    app: path.join(__dirname, 'main.js'),
+    app: path.join(__dirname, '/main.js'),
   },
   module: {
     rules: [{
-
+      test: /\.vue$/, // .vue로 끝나는 파일
+      loader: 'vue-loader',
     }],
   },
   plugins: [],
   output: {
     filename: '[name].js',
-    path: path.join(__dirname, 'dist'),
+    path: path.join(__dirname, '/dist'),
   },
 }
