7bc57460365e3801def84cda0689133b0731775c
BooGeon <qnrjs42@gmail.com>
2019-12-04 08:51:34 +0900

    5.가위바위보 준비

---
+++ 5.가위바위보/RockScissorsPaper.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    
+  </div>
+</template>
+
+<script>
+
+  export default {
+    data() {
+      return {
+
+      }
+    },
+    computed: { // 데이터를 계산같은거 할 때 성능적인 면에서 효과적임
+    },
+    methods: {
+
+    },
+  };
+</script>
+<!--
+  scoped는 vue의 컴포넌트에 유효한 css가 적용이 되고
+  scoped가 없으면 기본적인 css
+-->
+<style scoped>
+
+</style>
