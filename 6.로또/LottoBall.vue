e8b60a5a03ad8ed8d38c1bd48057ab37c3e4389b
BooGeon <qnrjs42@gmail.com>
2019-12-05 04:13:06 +0900

    LottoBall.vue 추가

---
+++ 6.로또/LottoBall.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="ball" :style="{background}">
+      {{number}}
+    </div>
+</template>
+
+<script>
+  export default {
+    name: 'LottoBall',
+    props: ['number'], // read-only
+    data() {
+      let background;
+      return {
+        background,
+      };
+    },
+  }
+</script>
+
+<style>
+  .ball {
+    display: inline-block;
+    border: 1px solid black;
+    border-radius: 20px;
+    width: 40px;
+    height: 40px;
+    line-height: 40px;
+    font-size: 20px;
+    text-align: center;
+    margin-right: 20px;
+  }
+</style>
