392e9a8419d79ca04f8256c8631d38ae355dc0f0
BooGeon <qnrjs42@gmail.com>
2019-12-06 01:09:12 +0900

    TableComponent.vue 생성

---
+++ 7.틱택토/TableComponent.vue
@@ -0,0 +1,17 @@
+<template>
+  <table>
+    <tr-component>
+
+    </tr-component>
+  </table>
+
+</template>
+
+<script>
+import TrComponents from './TrComponents';
+  export default {
+    components: {
+      TrComponents,
+    }
+  }
+</script>
