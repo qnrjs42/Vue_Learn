b857f32bbfa10ce92370bc141bac4b037a4ae867
BooGeon <qnrjs42@gmail.com>
2019-12-06 02:09:33 +0900

    this.$root(최상위 부모 컴포넌트 || TicTacToe), this.$parent(직속 부모 컴포넌트 || TrComponent)
    Vue.set(배열 데이터 수정할 때)
    문법 추가

--- 7.틱택토/TdComponent.vue
+++ 7.틱택토/TdComponent.vue
@@ -1,5 +1,5 @@
 <template>
-  <td>
+  <td @click="onClickTd">
     {{cellData}}
   </td>
 
@@ -12,5 +12,16 @@
       rowIndex: Number,
       cellIndex: Number,
     },
+    methods: {
+      onClickTd() {
+        //console.log(this.$root.$data); // 최상위 부모의 데이터(TicTacToe)
+        //console.log(this.$parent.$data); // 직속 부모의 데이터(TrComponent)
+        // this.$set을 써야 배열의 데이터 수정이 가능
+        const rootData = this.$root.$data;
+        this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
+        rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
+
+      }
+    }
   };
 </script>
--- 7.틱택토/TicTacToe.vue
+++ 7.틱택토/TicTacToe.vue
@@ -1,9 +1,14 @@
 <template>
-  <table-component :table-data="tableData" />
+  <div>
+    <table-component :table-data="tableData" />
+    <div>
+      {{turn}}님의 턴입니다.
+    </div>
+  </div>
 </template>
 
 <script>
-  import TableComponent from './Tablecomponent';
+  import TableComponent from './TableComponent';
   export default {
     components: {
       TableComponent
@@ -14,19 +19,24 @@
           ['', '', ''],
           ['', '', ''],
           ['', '', ''],
-        ]
+        ],
+        turn: 'O',
       };
     },
     computed: { // 데이터 계산같은거 할 때 성능적인 면에서 효과적임
     },
     methods: {
-
+      onChangeData() {
+        // this.tableData[1][0] = 'X'; 직접적으로 값이 바뀌지 않아 Vue.set을 써야함
+        // this.$set === Vue.set 동일
+        this.$set(this.tableData[1], 0, 'X');
+      }
     },
+
     mounted() { // 화면에 보여준 후
     },
 
     beforeDestroy() { // 사용한 라이프사이클 메모리 누수 위해 제거
-
     },
 
   };
@@ -35,6 +45,15 @@
   scoped는 vue의 컴포넌트에 유효한 css가 적용이 되고
   scoped가 없으면 기본적인 css
 -->
-<style scoped>
+<style>
+  table {
+    border-collapse: collapse;
+  }
+  td {
+    border: 1px solid black;
+    width: 40px;
+    height: 40px;
+    text-align: center;
+  }
 
 </style>
