86611549827deb9cca606f08c6d4ad599c3faa3b
BooGeon <qnrjs42@gmail.com>
2019-12-06 02:57:35 +0900

    틱택토 완성

--- 7.틱택토/TdComponent.vue
+++ 7.틱택토/TdComponent.vue
@@ -17,10 +17,49 @@
         //console.log(this.$root.$data); // 최상위 부모의 데이터(TicTacToe)
         //console.log(this.$parent.$data); // 직속 부모의 데이터(TrComponent)
         // this.$set을 써야 배열의 데이터 수정이 가능
+
+        // 칸에 두 번 클릭되었을 때 넘어감
+        if(this.cellData) return;
+
         const rootData = this.$root.$data;
         this.$set(rootData.tableData[this.rowIndex], this.cellIndex, rootData.turn);
-        rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
 
+        // 가로줄, 세로줄, 대각선 순서대로 3목이 되었는지 체크
+        let win = false;
+        if (rootData.tableData[this.rowIndex][0] === rootData.turn && rootData.tableData[this.rowIndex][1] === rootData.turn && rootData.tableData[this.rowIndex][2] === rootData.turn) {
+          win = true;
+        }
+        if (rootData.tableData[0][this.cellIndex] === rootData.turn && rootData.tableData[1][this.cellIndex] === rootData.turn && rootData.tableData[2][this.cellIndex] === rootData.turn) {
+          win = true;
+        }
+        if (rootData.tableData[0][0] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][2] === rootData.turn) {
+          win = true;
+        }
+        if (rootData.tableData[0][2] === rootData.turn && rootData.tableData[1][1] === rootData.turn && rootData.tableData[2][0] === rootData.turn) {
+          win = true;
+        }
+
+        if (win) { // 이긴 경우: 3줄 달성
+          rootData.winner = rootData.turn; // 승자 기록
+          rootData.turn = 'O'; // 데이터 초기화
+          rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']]; // 데이터 초기화
+        } else { // 무승부
+          let all = true; // all이 true면 무승부라는 뜻
+          rootData.tableData.forEach((row) => { // 무승부 검사
+            row.forEach((cell) => {
+              if (!cell) {
+                all = false;
+              }
+            });
+          });
+          if (all) { // 무승부
+            rootData.winner = '';
+            rootData.turn = 'O';
+            rootData.tableData = [['', '', ''], ['', '', ''], ['', '', '']];
+          } else {
+            rootData.turn = rootData.turn === 'O' ? 'X' : 'O';
+          }
+        }
       }
     }
   };
--- 7.틱택토/TicTacToe.vue
+++ 7.틱택토/TicTacToe.vue
@@ -4,6 +4,9 @@
     <div>
       {{turn}}님의 턴입니다.
     </div>
+    <div v-if="winner">
+      {{winner}}님의 승리!
+    </div>
   </div>
 </template>
 
@@ -21,6 +24,7 @@
           ['', '', ''],
         ],
         turn: 'O',
+        winner: '',
       };
     },
     computed: { // 데이터 계산같은거 할 때 성능적인 면에서 효과적임
