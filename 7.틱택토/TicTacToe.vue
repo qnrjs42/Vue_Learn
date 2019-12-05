93e7317f39a34688339d42ad40232207377c9e5c
BooGeon <qnrjs42@gmail.com>
2019-12-06 00:11:07 +0900

    7.틱택토 준비

---
+++ 7.틱택토/TicTacToe.vue
@@ -0,0 +1,37 @@
+<template>
+  <div> <!-- vue-css 태그는 카밀코드로 작성 예) backgroundImage yes background-image no -->
+
+  </div>
+</template>
+
+<script>
+  export default {
+    components: {
+
+    },
+    data() {
+      return {
+
+      };
+    },
+    computed: { // 데이터 계산같은거 할 때 성능적인 면에서 효과적임
+    },
+    methods: {
+
+    },
+    mounted() { // 화면에 보여준 후
+    },
+
+    beforeDestroy() { // 사용한 라이프사이클 메모리 누수 위해 제거
+
+    },
+
+  };
+</script>
+<!--
+  scoped는 vue의 컴포넌트에 유효한 css가 적용이 되고
+  scoped가 없으면 기본적인 css
+-->
+<style scoped>
+
+</style>
