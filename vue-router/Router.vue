7d9a28cdf464c6cf734e041212f0f414e17ddbde
BooGeon <qnrjs42@gmail.com>
2019-12-11 07:16:55 +0900

    Vue Router 설정

--- vue-router/Router.vue
+++ vue-router/Router.vue
@@ -1,6 +1,12 @@
 <template>
   <div>
-
+    <div>
+      <router-link to="/number-baseball">숫자야구</router-link>
+      <router-link to="/response-check">반응속도</router-link>
+      <router-link to="/rock-scissors-paper">가위바위보</router-link>
+      <router-link to="/lotto-generator">로또생성기</router-link>
+    </div>
+    <router-view></router-view>
   </div>
 </template>
 
--- vue-router/routes.js
+++ vue-router/routes.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import VueRouter from 'vue-router';
 import NumberBaseball from '../3.숫자야구/NumberBaseball';
 import ResponseCheck from '../4.반응속도체크/ResponseCheck';
+import RockScissorsPaper from '../5.가위바위보/RockScissorsPaper';
+import LottoGenerator from '../6.로또/LottoGenerator';
 
 Vue.use(VueRouter);
 
 export default new VueRouter ({
+  mode: 'history',
   routes: [
     { path: '/number-baseball', component: NumberBaseball },
     { path: '/response-check', component: ResponseCheck },
