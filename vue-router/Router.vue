d7f980d4247145908078f21ecd3bcfa41f5aabec
BooGeon <qnrjs42@gmail.com>
2019-12-12 03:30:17 +0900

    주소 쿼리스트링 기능 추가 -마무리-

--- vue-router/Router.vue
+++ vue-router/Router.vue
@@ -1,10 +1,11 @@
 <template>
   <div>
     <div>
-      <router-link to="/number-baseball">숫자야구</router-link>
-      <router-link to="/response-check">반응속도</router-link>
-      <router-link to="/rock-scissors-paper">가위바위보</router-link>
-      <router-link to="/lotto-generator">로또생성기</router-link>
+      <router-link to="/game/index">게임 매쳐</router-link>
+      <router-link to="/game/number-baseball">숫자야구</router-link>
+      <router-link to="/game/response-check">반응속도</router-link>
+      <router-link to="/game/rock-scissors-paper">가위바위보</router-link>
+      <router-link to="/game/lotto-generator">로또생성기</router-link>
     </div>
     <router-view></router-view>
   </div>
--- vue-router/routes.js
+++ vue-router/routes.js
@@ -4,6 +4,7 @@ import NumberBaseball from '../3.숫자야구/NumberBaseball';
 import ResponseCheck from '../4.반응속도체크/ResponseCheck';
 import RockScissorsPaper from '../5.가위바위보/RockScissorsPaper';
 import LottoGenerator from '../6.로또/LottoGenerator';
+import GameMatcher from './GameMatcher';
 
 Vue.use(VueRouter);
 
@@ -14,5 +15,6 @@ export default new VueRouter ({
     { path: '/response-check', component: ResponseCheck },
     { path: '/rock-scissors-paper', component: RockScissorsPaper },
     { path: '/lotto-generator', component: LottoGenerator },
+    { path: '/game/:name', component:GameMatcher }, // /game/NumberBaseball
   ],
 });
